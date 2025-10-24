55aae53a4daca768a9e066488a2f875292a513d7
MoniRudashki-dev <s.rudashki@webrika.bg>
2025-10-24 17:27:29 +0300

    make menu for mobile

--- obichaite-se/src/Header/Component.client.tsx
+++ obichaite-se/src/Header/Component.client.tsx
@@ -9,16 +9,14 @@ import { useAppDispatch } from '@/hooks/redux-hooks'
 import { GenericImage, GenericParagraph } from '@/components/Generic'
 import { ArrowIcon, MenuIcon, SearchLogo, ShoppingCartIcon, UserProfileIcon } from '@/assets/icons'
 import { setOpenSearch } from '@/store/features/root'
+import Menu from './Menu'
 
 const HeaderClient = ({ headerData }: { headerData: DataFromGlobalSlug<'header'> }) => {
   const dispatch = useAppDispatch()
   const { categoryItems, logo } = headerData as Header
 
-  const [searchOpen, setSearchOpen] = useState(false)
   const [openMenu, setOpenMenu] = useState(false)
   const [openCategoryIndex, setOpenCategoryIndex] = useState(-1)
-
-  const closeSearchHandler = useCallback(() => setSearchOpen(false), [])
 
   useEffect(() => {
     if (openMenu) {
@@ -99,132 +97,146 @@ const HeaderClient = ({ headerData }: { headerData: DataFromGlobalSlug<'header'>
     : []
 
   return (
-    <header
-      className="w-full fixed z-[11] top-0 left-0 right-0 flex flex-col bg-white"
-      onMouseLeave={() => setOpenCategoryIndex(-1)}
-    >
-      <nav
-        className="w-full content_wrapper_mobile-full bg-white flex justify-between items-center h-[52px] md:h-[70px]"
-        // style={{ overflowX: 'visible', overflowY: 'visible' }}
-        onMouseEnter={() => setOpenCategoryIndex(-1)}
+    <>
+      <div className="landscape:hidden portrait:block">
+        <Menu openMenu={openMenu} setOpenMenu={setOpenMenu} categoryItems={categoryItems} />
+      </div>
+      <header
+        className="w-full fixed z-[11] top-0 left-0 right-0 flex flex-col bg-white"
+        onMouseLeave={() => setOpenCategoryIndex(-1)}
       >
-        <Link href={'/'} aria-label="Отиди на начална страница">
-          <GenericImage
-            src={(logo as Media)?.url as string}
-            alt={(logo as Media)?.alt as string}
-            wrapperClassName="w-[116px] h-[41px] md:w-[158px] md:h-[50px] flex items-center justify-center relative"
-            imageClassName="w-full h-full object-contain"
-            fill={true}
-          />
-        </Link>
-
-        <ul className="flex items-center gap-2 px-2">
-          <li>
-            <Link href={'/login'} aria-label="Към вход" className="flex items-center gap-2">
-              <GenericParagraph
-                pType="regular"
-                extraClass="hidden md:block"
-                fontStyle="font-sansation font-[700]"
-              >
-                Вход
-              </GenericParagraph>
-              <button
-                className="w-[32px] h-[32px] md:w-[48px] md:h-[48px] rounded-full flex justify-center items-center border-[1px] border-brown p-[5px]"
-                aria-label="Потребител / Вход"
-                title="Потребител"
-              >
-                <UserProfileIcon />
-              </button>
-            </Link>
-          </li>
-          <li>
-            <button
-              className="w-[32px] h-[32px] md:w-[48px] md:h-[48px] rounded-full flex justify-center items-center border-[1px] border-brown pb-[7px] pt-[5px]"
-              aria-label="Търсене на продукт"
-              title="Търсене на продукт"
-              onClick={() => {
-                dispatch(setOpenSearch(true))
-              }}
-            >
-              <SearchLogo />
-            </button>
-          </li>
-          <li className="flex items-center gap-2">
-            <div className="hidden flex-col md:flex">
-              <GenericParagraph pType="small" textColor="text-brown">
-                Количка
-              </GenericParagraph>
-              {/* //TODO dynamic price */}
-              <GenericParagraph pType="small" textColor="text-bordo" extraClass="tracking-tighter">
-                0.00 BGN
-              </GenericParagraph>
-            </div>
-            <button
-              className="w-[32px] h-[32px] md:w-[48px] md:h-[48px] rounded-full flex justify-center items-center border-[1px] border-brown p-[3px] relative"
-              aria-label="Потребител количка"
-              title="Потребител количка"
-            >
-              <div
-                className="absolute z-[2] top-[-5px] right-[-5px] w-[16px] h-[16px] md:w-[20px] md:h-[20px] rounded-full bg-bordo text-white
-              flex justify-center items-center"
-              >
-                {/* //TODO dynamic count */}
-                <p className="text-white font-sensation font-[700] text-[10px] md:text-[12px]">0</p>
-              </div>
-              <ShoppingCartIcon />
-            </button>
-          </li>
-          <li className="md:hidden">
-            <button
-              className="w-[34px] h-[34px] rounded-full flex justify-center items-center border-[1px] border-brown pb-[7px] pt-[5px]"
-              aria-label="Меню отваряне"
-              title="Меню отваряне"
-            >
-              {/* //TODO change the icon */}
-              <MenuIcon />
-            </button>
-          </li>
-        </ul>
-      </nav>
-      <nav className="w-full bg-brown justify-between items-center h-[70px] hidden md:flex">
-        <ul
-          className="content_wrapper_mobile-full w-full flex justify-between items-center gap-2 px-2"
-          aria-label="Навигация на категории"
+        <nav
+          className="w-full content_wrapper_mobile-full bg-white flex justify-between items-center h-[52px] md:h-[70px]"
+          // style={{ overflowX: 'visible', overflowY: 'visible' }}
+          onMouseEnter={() => setOpenCategoryIndex(-1)}
         >
-          {linksContent}
-        </ul>
-      </nav>
-      <div
-        className={`w-full h-[400px] bg-white absolute top-full left-0 right-0 z-[-1] border-b-[1px] border-brown
+          <Link href={'/'} aria-label="Отиди на начална страница">
+            <GenericImage
+              src={(logo as Media)?.url as string}
+              alt={(logo as Media)?.alt as string}
+              wrapperClassName="w-[116px] h-[41px] md:w-[158px] md:h-[50px] flex items-center justify-center relative"
+              imageClassName="w-full h-full object-contain"
+              fill={true}
+            />
+          </Link>
+
+          <ul className="flex items-center gap-2 px-2">
+            <li>
+              <Link href={'/login'} aria-label="Към вход" className="flex items-center gap-2">
+                <GenericParagraph
+                  pType="regular"
+                  extraClass="hidden md:block"
+                  fontStyle="font-sansation font-[700]"
+                >
+                  Вход
+                </GenericParagraph>
+                <button
+                  className="w-[32px] h-[32px] md:w-[48px] md:h-[48px] rounded-full flex justify-center items-center border-[1px] border-brown p-[5px]"
+                  aria-label="Потребител / Вход"
+                  title="Потребител"
+                >
+                  <UserProfileIcon />
+                </button>
+              </Link>
+            </li>
+            <li>
+              <button
+                className="w-[32px] h-[32px] md:w-[48px] md:h-[48px] rounded-full flex justify-center items-center border-[1px] border-brown pb-[7px] pt-[5px]"
+                aria-label="Търсене на продукт"
+                title="Търсене на продукт"
+                onClick={() => {
+                  dispatch(setOpenSearch(true))
+                }}
+              >
+                <SearchLogo />
+              </button>
+            </li>
+            <li className="flex items-center gap-2">
+              <div className="hidden flex-col md:flex">
+                <GenericParagraph pType="small" textColor="text-brown">
+                  Количка
+                </GenericParagraph>
+                {/* //TODO dynamic price */}
+                <GenericParagraph
+                  pType="small"
+                  textColor="text-bordo"
+                  extraClass="tracking-tighter"
+                >
+                  0.00 BGN
+                </GenericParagraph>
+              </div>
+              <button
+                className="w-[32px] h-[32px] md:w-[48px] md:h-[48px] rounded-full flex justify-center items-center border-[1px] border-brown p-[3px] relative"
+                aria-label="Потребител количка"
+                title="Потребител количка"
+              >
+                <div
+                  className="absolute z-[2] top-[-5px] right-[-5px] w-[16px] h-[16px] md:w-[20px] md:h-[20px] rounded-full bg-bordo text-white
+              flex justify-center items-center"
+                >
+                  {/* //TODO dynamic count */}
+                  <p className="text-white font-sensation font-[700] text-[10px] md:text-[12px]">
+                    0
+                  </p>
+                </div>
+                <ShoppingCartIcon />
+              </button>
+            </li>
+            <li className="md:hidden">
+              <button
+                className="w-[34px] h-[34px] rounded-full flex justify-center items-center border-[1px] border-brown pb-[7px] pt-[5px]"
+                aria-label="Меню отваряне"
+                title="Меню отваряне"
+                onClick={() => {
+                  setOpenMenu(true)
+                }}
+              >
+                {/* //TODO change the icon */}
+                <MenuIcon />
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <nav className="w-full bg-brown justify-between items-center h-[70px] hidden md:flex">
+          <ul
+            className="content_wrapper_mobile-full w-full flex justify-between items-center gap-2 px-2"
+            aria-label="Навигация на категории"
+          >
+            {linksContent}
+          </ul>
+        </nav>
+        <div
+          className={`w-full h-[400px] bg-white absolute top-full left-0 right-0 z-[-1] border-b-[1px] border-brown
           transition-[transform,opacity] duration-500 ease-in-out p-6 white_background_forms ${
             openCategoryIndex > -1
               ? 'translate-y-0 opacity-100 pointer-events-auto'
               : 'translate-y-[-400px] opacity-0 pointer-events-none'
           }`}
-      >
-        <div className="w-full h-full content_wrapper flex justify-stretch items-stretch">
-          <nav className="w-[75%] h-full p-6">
-            <ul className="w-full h-full flex flex-wrap gap-2 max-h-[376px] overflow-y-auto">
-              {childrenContent}
-            </ul>
-          </nav>
-          <div className="w-[25%] min-w-[25%] h-full p-6">
-            {currentMedia?.url && (
-              <GenericImage
-                src={currentMedia?.url || ''}
-                alt={currentMedia?.alt || ''}
-                wrapperClassName="w-full h-full aspect-square rounded-[16px] overflow-hidden relative"
-                imageClassName="w-full h-full object-cover"
-                fill={true}
-                focalX={currentMedia?.focalX || 50}
-                focalY={currentMedia?.focalY || 50}
-                sizes="512px"
-              />
-            )}
+        >
+          <div className="w-full h-full content_wrapper flex justify-stretch items-stretch">
+            <nav className="w-[75%] h-full p-6">
+              <ul className="w-full h-full flex flex-wrap gap-2 max-h-[376px] overflow-y-auto">
+                {childrenContent}
+              </ul>
+            </nav>
+            <div className="w-[25%] min-w-[25%] h-full p-6">
+              {currentMedia?.url && (
+                <GenericImage
+                  src={currentMedia?.url || ''}
+                  alt={currentMedia?.alt || ''}
+                  wrapperClassName="w-full h-full aspect-square rounded-[16px] overflow-hidden relative"
+                  imageClassName="w-full h-full object-cover"
+                  fill={true}
+                  focalX={currentMedia?.focalX || 50}
+                  focalY={currentMedia?.focalY || 50}
+                  sizes="512px"
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   )
 }
 
---
+++ obichaite-se/src/Header/Menu.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import { ArrowIcon, CloseCircle } from '@/assets/icons'
+import { GenericParagraph } from '@/components/Generic'
+import { Header } from '@/payload-types'
+import { generateHref, LinkObject } from '@/utils/generateHref'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const Menu = ({
+  openMenu,
+  setOpenMenu,
+  categoryItems,
+}: {
+  openMenu: boolean
+  setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>
+  categoryItems: Header['categoryItems']
+}) => {
+  const [openCategoryIndex, setOpenCategoryIndex] = useState(-1)
+
+  const linksContent = categoryItems?.map((item, i) => {
+    if (!item?.link?.label) return null
+
+    const childrenContent = !!item.children
+      ? item.children.map((childItem) => {
+          return (
+            <li key={childItem.id} className="w-full py-2 bg-pink/20 my-[2px]">
+              <Link
+                href={generateHref(childItem as LinkObject)}
+                aria-label={childItem?.link?.label}
+                target={childItem?.link?.newTab ? '_blank' : '_self'}
+                prefetch={true}
+                className="w-full h-full"
+              >
+                <button className="w-fit pl-4 h-full">
+                  <span className="text-[16px] text-right font-kolka font-[500] text-brown transition-colors duration-500">
+                    {childItem?.link?.label}
+                  </span>
+                </button>
+              </Link>
+            </li>
+          )
+        })
+      : []
+
+    return (
+      <li className="w-full px-2 py-2 flex flex-col" key={`${item?.link?.label}${i}`}>
+        <div className="flex justify-between items-center border-b-[1px] border-dashed border-brown/80">
+          <Link
+            href={generateHref(item as LinkObject)}
+            aria-label={item?.link?.label}
+            target={item?.link?.newTab ? '_blank' : '_self'}
+            className=""
+            prefetch={true}
+          >
+            <p className="font-sansation font-[700] italic text-[18px] text-brown">
+              {item?.link?.label}
+            </p>
+          </Link>
+
+          <button
+            className={`w-[20px] h-[20px] flex justify-center items-center
+                transition-transform duration-300 ease-in-out ${
+                  openCategoryIndex === i + 1 ? 'rotate-[-90deg]' : 'rotate-[90deg]'
+                }`}
+            onClick={() => {
+              if (openCategoryIndex === i + 1) {
+                setOpenCategoryIndex(-1)
+              } else {
+                setOpenCategoryIndex(i + 1)
+              }
+            }}
+          >
+            <ArrowIcon />
+          </button>
+        </div>
+
+        {openCategoryIndex === i + 1 && (
+          <ul className="w-full flex flex-col mt-2">{childrenContent}</ul>
+        )}
+      </li>
+    )
+  })
+
+  return (
+    <div
+      className={`fixed right-0 top-0 w-[calc(100%-48px)] h-screen overflow-y-auto bg-white z-[13] white_background_bubble
+     ${openMenu ? 'translate-x-0' : 'translate-x-full'} transition-[transform] duration-500 ease-in-out
+    `}
+    >
+      <div className="w-full flex items-center justify-between py-2 px-4 border-b-[1px] border-bordo/80">
+        <GenericParagraph
+          fontStyle="font-sansation font-[700]"
+          pType="large"
+          textColor="text-bordo"
+        >
+          Меню
+        </GenericParagraph>
+
+        <button
+          onClick={() => setOpenMenu(false)}
+          className="w-[28px] h-[28px] flex justify-center items-center"
+        >
+          <CloseCircle />
+        </button>
+      </div>
+
+      <ul className="w-full flex flex-col gap-3 py-3">{linksContent}</ul>
+    </div>
+  )
+}
+
+export default Menu
