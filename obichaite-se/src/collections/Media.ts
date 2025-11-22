4a7d905cb95116b3e6709b3258c33a70ba8d4325
MoniRudashki-dev <s.rudashki@webrika.bg>
2025-11-22 08:06:56 +0200

    fix image upload

--- obichaite-se/src/collections/Media.ts
+++ obichaite-se/src/collections/Media.ts
@@ -3,6 +3,10 @@ import path from 'path'
 import { uploadToSuperhosting } from '@/lib/uploadToHost'
 import { unlink } from 'fs/promises'
 
+const uploadStaticDir = process.env.NODE_ENV === 'production' ? '/tmp/media' : 'media'
+const uploadLocalDir =
+  process.env.NODE_ENV === 'production' ? '/tmp/media' : path.join(process.cwd(), 'media')
+
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
@@ -19,6 +23,7 @@ export const Media: CollectionConfig = {
   upload: {
     adminThumbnail: 'thumbnail',
     focalPoint: true,
+    staticDir: uploadStaticDir,
   },
   hooks: {
     afterChange: [
@@ -27,20 +32,14 @@ export const Media: CollectionConfig = {
         if (!doc?.filename) return doc
 
         // локалният път до файла, който Payload е записал
-        const localPath = path.join(
-          process.cwd(),
-          'media', // ако staticDir е друга – смени и тук
-          doc.filename as string,
-        )
+        const localPath = path.join(uploadLocalDir, doc.filename as string)
 
         try {
           await uploadToSuperhosting(localPath, doc.filename as string)
           await unlink(localPath).catch((err) => {
             console.error('Error deleting local file:', err)
           })
-          return {
-            ...doc,
-          }
+          return doc
         } catch (err) {
           console.error('Error uploading to Superhosting:', err)
           return doc
