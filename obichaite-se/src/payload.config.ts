508042db11c43731e1043dd54e7a75576207139a
MoniRudashki-dev <s.rudashki@webrika.bg>
2025-11-24 15:21:46 +0200

    remove push ot database

--- obichaite-se/src/payload.config.ts
+++ obichaite-se/src/payload.config.ts
@@ -65,7 +65,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
-    push: true,
+    push: false,
   }),
   sharp,
   plugins: [payloadCloudPlugin(), ...plugins],
