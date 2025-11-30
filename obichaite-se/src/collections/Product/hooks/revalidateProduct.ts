db169c320c4c40e717d649389e0b02e6defbda22
MoniRudashki-dev <s.rudashki@webrika.bg>
2025-11-30 11:00:23 +0200

    updates on revalidate product hook

--- obichaite-se/src/collections/Product/hooks/revalidateProduct.ts
+++ obichaite-se/src/collections/Product/hooks/revalidateProduct.ts
@@ -8,90 +8,74 @@ export const revalidateProduct: CollectionAfterChangeHook<Product> = async ({
   previousDoc,
   req: { payload, context },
 }) => {
+  const revalidateAll = async () => {
+    if (doc.isInThematic) {
+      revalidatePath('/kategorii/tematichni-podarytsi')
+    }
+
+    const category =
+      (await payload.find({
+        collection: 'category',
+        where: {
+          id: {
+            equals: doc.category,
+          },
+        },
+      })) || null
+
+    const subCategory =
+      (await payload.find({
+        collection: 'sub-category',
+        where: {
+          id: {
+            equals: doc.subCategory,
+          },
+        },
+      })) || null
+
+    const otherSubCategories = await Promise.all(
+      (doc.otherSubCategories || []).map(async (subCatId) => {
+        const res = await payload.find({
+          collection: 'sub-category',
+          where: {
+            id: {
+              equals: subCatId,
+            },
+          },
+        })
+        return res.docs[0]
+      }),
+    )
+    revalidatePath(`/kategorii/${category.docs[0].slug}`)
+    revalidatePath(`/kategorii/${subCategory.docs[0].slug}`)
+    otherSubCategories?.forEach((subCat) => {
+      revalidatePath(`/kategorii/${subCat.slug}`)
+    })
+    revalidateTag('produkt-sitemap')
+  }
+
   // console.log('Revalidate Product Hook Triggered', doc)
   if (!context.disableRevalidate) {
     if (doc._status === 'published') {
       const path = `/produkt/${doc.slug}`
 
+      console.log('REVALIDATE ALL')
+
       payload.logger.info(`Revalidating post at path: ${path}`)
 
+      revalidateAll()
       revalidatePath(path)
-      revalidateTag('produkt-sitemap')
     }
 
     // If the post was previously published, we need to revalidate the old path
     if (previousDoc._status === 'published' && doc._status !== 'published') {
       const oldPath = `/produkt/${previousDoc.slug}`
 
-      console.log('REVALIDATE ALL')
-
       payload.logger.info(`Revalidating old post at path: ${oldPath}`)
 
       revalidatePath(oldPath)
-      revalidateTag('produkt-sitemap')
 
-      // revalidate category page
-
-      if (doc.isInThematic) {
-        revalidatePath('/kategorii/tematichni-podarytsi')
-      }
-
-      // console.log('Revalidating product doc:', doc)
-      const category =
-        (await payload.find({
-          collection: 'category',
-          where: {
-            id: {
-              equals: doc.category,
-            },
-          },
-        })) || null
-
-      const subCategory =
-        (await payload.find({
-          collection: 'sub-category',
-          where: {
-            id: {
-              equals: doc.subCategory,
-            },
-          },
-        })) || null
-
-      const otherSubCategories = await Promise.all(
-        (doc.otherSubCategories || []).map(async (subCatId) => {
-          const res = await payload.find({
-            collection: 'sub-category',
-            where: {
-              id: {
-                equals: subCatId,
-              },
-            },
-          })
-          return res.docs[0]
-        }),
-      )
-
-      // console.log('Revalidating category:', category)
-      payload.logger.info(`Revalidating old post at path: ${`/kategorii/${category.docs[0].slug}`}`)
-      revalidatePath(`/kategorii/${category.docs[0].slug}`)
-
-      // revalidate subCategory
-      // console.log('Revalidating subCategory:', subCategory)
-      payload.logger.info(
-        `Revalidating old post at path: ${`/kategorii/${subCategory.docs[0].slug}`}`,
-      )
-      revalidatePath(`/kategorii/${subCategory.docs[0].slug}`)
-
-      // revalidate otherSubCategories
-      otherSubCategories?.forEach((subCat) => {
-        // console.log('Revalidating otherSubCategory:', subCat)
-        payload.logger.info(`Revalidating old post at path: ${`/kategorii/${subCat.slug}`}`)
-        revalidatePath(`/kategorii/${subCat.slug}`)
-      })
-
-      revalidateTag('produkt-sitemap')
-
-      revalidateTag('category-sitemap')
+      revalidateAll()
     }
   }
 
@@ -102,15 +86,11 @@ export const revalidateDeleteProduct: CollectionAfterDeleteHook<Product> = async
   doc,
   req: { payload, context },
 }) => {
-  // console.log('Revalidate Delete Product Hook Triggered')
-  // console.log('Deleted context:', context)
   if (!context.disableRevalidate && doc.slug) {
     const path = `/produkt/${doc?.slug}`
 
     revalidatePath(path)
-    // revalidate category page
 
-    // console.log('Revalidating product doc:', doc)
     const category = await payload.find({
       collection: 'category',
       where: {
@@ -143,20 +123,15 @@ export const revalidateDeleteProduct: CollectionAfterDeleteHook<Product> = async
       }),
     )
 
-    // console.log('Revalidating category:', category)
     payload.logger.info(`Revalidating old post at path: ${`/kategorii/${category.docs[0].slug}`}`)
     revalidatePath(`/kategorii/${category.docs[0].slug}`)
 
-    // revalidate subCategory
-    // console.log('Revalidating subCategory:', subCategory)
     payload.logger.info(
       `Revalidating old post at path: ${`/kategorii/${subCategory.docs[0].slug}`}`,
     )
     revalidatePath(`/kategorii/${subCategory.docs[0].slug}`)
 
-    // revalidate otherSubCategories
     otherSubCategories?.forEach((subCat) => {
-      // console.log('Revalidating otherSubCategory:', subCat)
       payload.logger.info(`Revalidating old post at path: ${`/kategorii/${subCat.slug}`}`)
       revalidatePath(`/kategorii/${subCat.slug}`)
     })
